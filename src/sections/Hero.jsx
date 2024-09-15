aee220c70de8b39a1ec31ec23e3bcfaa488fbd96
Aayan Joshi <aayanjoshi@Aayans-Mac-mini.local>
2024-09-15 08:02:44 +0545

    Implement Hacker Room #D Model

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,51 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import HackerRoom from '../components/HackerRoom.jsx';
+import CanvasLoader from '../components/CanvasLoader.jsx';
+import { useMediaQuery } from 'react-responsive';
+import { calculateSizes } from '../constants/index.js';
+import Target from '../components/Target.jsx';
+import ReactLogo from '../components/ReactLogo.jsx';
+
+const Hero = () => {
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+    return (
+        <section className={`min-h-screen w-full flex flex-col relative`}>
+            <div className={`w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3`}>
+                <p className={`sm:text-3xl text-2xl text-white text-center font-generalsans`}>
+                    Hi, I am Aayan<span className={`waving-hand`}>👋</span>
+                </p>
+                <p className={`hero_tag text-gray_gradient`}>Building Products & Brands</p>
+            </div>
+
+            <div className={`w-full h-full absolute inset-0`}>
+                <Canvas className={`w-full h-full`}>
+                    <Suspense fallback={<CanvasLoader />}>
+                        <perspectiveCamera makeDefault position={[20, 20, 100]} near={0.1} far={1000} />
+
+                        <HackerRoom
+                            position={sizes.deskPosition}
+                            rotation={[0, -Math.PI, 0]}
+                            scale={sizes.deskScale}
+                        />
+
+                        <group>
+                            <Target position={sizes.targetPosition} />
+                            <ReactLogo position={sizes.reactLogoPosition} />
+                        </group>
+
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                    </Suspense>
+                </Canvas>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
